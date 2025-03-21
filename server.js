cdbce9d8008fd065e992d3f70065bcac80cde30b
urigwesomto201 <coryguerrero500@gmail.com>
2025-03-21 07:25:57 -0500

    somto1

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express= require('express');
 require('./config/database');
 const PORT = 5527
+const cors = require('cors')
 const weatherrouter =require('./routes/weather')
 const app = express()
 app.use(express.json());
 app.use(weatherrouter);
+app.use(cors())
 app.listen(PORT,()=>{
   console.log(`server listenimg to ${PORT}`);
   
