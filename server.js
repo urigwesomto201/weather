25e2fff500f397bb614f8c330c89b6c6bcb31ddd
urigwesomto201 <coryguerrero500@gmail.com>
2025-03-22 14:16:17 -0500

    somto1

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express= require('express');
 require('./config/database');
 const PORT = 5527
+const morgan = require('morgan')
 const cors = require('cors')
 const weatherrouter =require('./routes/weather')
 const app = express()
 app.use(express.json());
 app.use(weatherrouter);
 app.use(cors())
+app.use(morgan('dev'));
 app.listen(PORT,()=>{
   console.log(`server listenimg to ${PORT}`);
   
