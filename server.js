066e38da1d6151dd9d7fd05da9ff0e49b46bd996
urigwesomto201 <coryguerrero500@gmail.com>
2025-03-23 07:48:20 -0500

    morgan2

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const weatherrouter =require('./routes/weather')
 const app = express()
+
+app.use(cors({origin:"*"}))
 app.use(express.json());
 app.use(weatherrouter);
-app.use(cors({origin:"*"}))
 app.use(morgan('dev'));
 app.listen(PORT,()=>{
   console.log(`server listenimg to ${PORT}`);
